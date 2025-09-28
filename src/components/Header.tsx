3e059c543879d4af7ad60edcdd49f39dc02186bd
Muhammad Rizwan Aslam <mianrizwan465@gmail.com>
2025-09-28 17:37:31 +0500

    update logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import logoImg from "../assets/logo.png";
 
 const Header: React.FC = () => {
   return (
@@ -7,10 +8,11 @@ const Header: React.FC = () => {
       <div className="container mx-auto px-6 py-4 flex justify-between items-center">
         <div className="text-2xl font-bold text-white">
           <a href="#" className="flex items-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 text-brand-teal-400" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M5 3a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2V5a2 2 0 00-2-2H5zm0 2h10v7h-2V7a1 1 0 00-1-1H8V5z" clipRule="evenodd" />
-            </svg>
-            Capital Chai
+            <img
+              src={logoImg}
+              alt="hyprlynx-logo"
+              className="h-20 object-cover logo-img"
+            />
           </a>
         </div>
         <nav className="hidden md:flex space-x-8">
@@ -20,7 +22,7 @@ const Header: React.FC = () => {
           <a href="#faq" className="text-gray-300 hover:text-brand-teal-400 transition-colors duration-300">FAQ</a>
         </nav>
         <a href="#submit-idea" className="hidden md:inline-block bg-brand-teal-500 hover:bg-brand-teal-600 text-white font-bold py-2 px-4 rounded-lg transition-transform duration-300 hover:scale-105">
-          Submit Idea
+          Submit Product
         </a>
       </div>
     </header>
