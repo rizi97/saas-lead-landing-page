d5118f05da420f011f594be58fcacb531e13f8ce
Muhammad Rizwan Aslam <mianrizwan465@gmail.com>
2025-10-02 18:46:30 +0500

    update changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: 'hyprlynxcapital.com', // 👈 apna GitHub repo name likhna
+  base: '/saas-lead-landing-page', // 👈 apna GitHub repo name likhna
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
