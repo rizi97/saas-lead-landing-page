19e0f89b562afc87c1025231c11f62560fb22cb1
Muhammad Rizwan Aslam <mianrizwan465@gmail.com>
2025-10-02 19:09:21 +0500

    update changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/saas-lead-landing-page', // 👈 apna GitHub repo name likhna
+  base: '/', 
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
